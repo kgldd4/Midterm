8e7de033218b4ad687c7f45bc51bbdf41b9b269a
kgldd4 <92762045+kgldd4@users.noreply.github.com>
2021-10-18 22:12:01 -0500

    Update Turtleimage.py

--- Turtleimage.py
+++ Turtleimage.py
@@ -1,3 +1,4 @@
+#this code is referenced from https://codepen.io/Recidvst/pen/EyxYmG
 <?xml version="1.0" encoding="utf-8"?>
 <svg viewBox="13 -4 461 455" xmlns="http://www.w3.org/2000/svg" xmlns:bx="https://www.boxy-svg.com/bx">
   <g>
