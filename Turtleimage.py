4437e290cc735886e5182167319657e69eb92242
kgldd4 <92762045+kgldd4@users.noreply.github.com>
2021-10-18 22:02:13 -0500

    Add files via upload

---
+++ Turtleimage.py
@@ -0,0 +1,29 @@
+#I learned how to code this using https://youtu.be/p7CiFhiTdvY 
+import turtle
+
+bingobongo = turtle.Turtle()
+bingobongo.color('yellow')
+bingobongo.pensize(5)
+bingobongo.shape('turtle')
+
+bingobongo.forward(50)
+bingobongo.left(36)
+bingobongo.forward(50)
+bingobongo.left(36)
+bingobongo.forward(50)
+bingobongo.left(36)
+bingobongo.forward(50)
+bingobongo.left(36)
+bingobongo.forward(50)
+bingobongo.left(36)
+bingobongo.forward(50)
+bingobongo.left(36)
+bingobongo.forward(50)
+bingobongo.left(36)
+bingobongo.forward(50)
+bingobongo.left(36)
+bingobongo.forward(50)
+bingobongo.left(36)
+bingobongo.forward(50)
+
+
